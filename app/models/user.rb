0bcbbca3d98e819f789321c1eb4b358a40f2037a
= <=>
2021-07-19 01:14:24 +1000

    added validation of uniqueness to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
     has_one :budget, dependent: :destroy
 
     validates_presence_of :name, :email, :password_digest
+    validates :email, uniqueness: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should have_many(:transactions).dependent(:destroy) }
+  
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:category_type) }
 end
