5f4e0a5ca7bc7d3d4c495b1e15de78801e089521
= <=>
2021-07-19 13:28:39 +1000

    added income action and income categories representer

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,7 +1,12 @@
 module Api
     module V1
         class CategoriesController < ApplicationController
-           
+           def income
+           income_categories = Category.select { |entry| entry.category_type == "income" }
+
+           render json: IncomeCategoriesRepresenter.new(income_categories).as_json
+           end
+        
         end
     end
 end
