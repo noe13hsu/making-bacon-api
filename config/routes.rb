f89366c82c873903d56bbad4d63c9d59e50b4600
Noe <74361913+noe13hsu@users.noreply.github.com>
2021-07-20 20:57:47 +1000

    fixed transaction bug and tests, added seeds, and updated routes (#18)
    Co-authored-by: = <=>

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :transactions, path: '/user/transactions'
-      resources :categories, path: '/user/categories'
+      resources :transactions, path: '/user/:user_id/transactions'
+      resources :categories, path: '/user/:user_id/categories'
+      resources :budgets, path: '/user/:user_id/budgets'
 
       # post 'categories', to: 'categories#create'
-      get '/user/income_categories', to: 'categories#income'
-      get '/user/expense_categories', to: 'categories#expense'
+      get '/user/:user_id/income_categories', to: 'categories#income'
+      get '/user/:user_id/expense_categories', to: 'categories#expense'
 
-      get '/user/expense_transactions', to: 'transactions#expense'
-      get '/user/income_transactions', to: 'transactions#income'
+      get '/user/:user_id/expense_transactions', to: 'transactions#expense'
+      get '/user/:user_id/income_transactions', to: 'transactions#income'
+
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
