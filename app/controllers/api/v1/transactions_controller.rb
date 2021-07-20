b20dc8c95596587f8ac1926c7525c8634cb1bdae
Noe <74361913+noe13hsu@users.noreply.github.com>
2021-07-20 14:03:54 +1000

    added transaction controller actions, representers and routes (#15)
    Co-authored-by: = <=>

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,7 +1,66 @@
 module Api
     module V1
         class TransactionsController < ApplicationController
-           
+            before_action :set_transaction, only: [:show, :update, :destroy]
+
+            rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+            def income
+                income_transactions = Transaction.select { |entry| entry.category.category_type == "income" }
+
+                render json: TransactionsRepresenter.new(income_transactions).as_json
+            end
+
+            def expense
+                expense_transactions = Transaction.select { |entry| entry.category.category_type == "expense" }
+
+                render json: TransactionsRepresenter.new(expense_transactions).as_json
+            end
+
+            def show
+                transaction = @transaction
+
+                render json: TransactionRepresenter.new(transaction).as_json
+            end
+
+            def create
+                transaction = Transaction.new(transaction_params)
+
+                if transaction.save
+                    render json: TransactionRepresenter.new(transaction).as_json, status: :created
+                else
+                    render json: { error: "Failed to create transaction" }, status: :unprocessable_entity
+                end
+            end
+
+            def update
+                transaction = @transaction
+                if transaction.update(transaction_params)
+                    render json: TransactionRepresenter.new(transaction).as_json
+                else
+                    render json: { error: "Failed to update transaction" }, status: :unprocessable_entity
+                end
+            end
+
+            def destroy
+                @transaction.destroy!
+
+                head :no_content
+            end
+
+            private
+
+            def set_transaction
+                @transaction = Transaction.find(params[:id])
+            end
+
+            def transaction_params
+                params.require(:transaction).permit(:category_id, :description, :amount, :date)
+            end
+
+            def record_not_found(e)
+                render json: { error: e.message }, status: :not_found
+            end
         end
     end
 end
