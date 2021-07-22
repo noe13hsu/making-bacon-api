e527fd42727d4ca54fb85bb98dc8d24c134e5dbf
= <=>
2021-07-22 23:22:07 +1000

    added authentication

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,6 +1,7 @@
 module Api
     module V1
         class CategoriesController < ApplicationController
+            before_action :authenticate
             before_action :current_user_category_selection, only: [:show, :update, :destroy]
 
             def index
--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -3,7 +3,7 @@ class AuthenticationTokenService
     ALGORITHM_TYPE = 'HS256'
 
     def self.call(user_id)
-        payload = {user_id: user_id}
+        payload = {user_id: user_id, exp: Time.now.to_i + 4 * 3600 }
 
         JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
     end
